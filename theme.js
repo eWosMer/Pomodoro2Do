009e3ece8e556b3c069258093410ce3f3e9c289a
eWosMer <aymuhammedmert@gmail.com>
2023-04-24 03:38:32 +0300

    initialize repository

---
+++ theme.js
@@ -0,0 +1,9 @@
+const temalar = [pastel,light,dark,cupcake,bumblebee,emerald,corporate,
+                synthwave,retro,cyberpunk,valentine,halloween,garden,forest,
+                aqua,wireframe,black,luxury,draclua,cmyk,autumn,business,acid,
+                lemonade,night,coffee,];
+
+
+//black oldu ama logolar beyaz olabilir!?
+//luxury çıkartılabilir
+//26 tane var
