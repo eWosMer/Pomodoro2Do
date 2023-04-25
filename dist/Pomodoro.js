570d577f3dea4f2e434263a56fab936874feaffd
eWosMer <aymuhammedmert@gmail.com>
2023-04-25 04:07:27 +0300

    Menü oluşturuldu,kronometre-sayaç geçişi ayarlandı

--- dist/Pomodoro.js
+++ dist/Pomodoro.js
@@ -9,12 +9,109 @@ let calisma_vakti = true;
 let pomodoro_baslangic=25;
 let mola_baslangic=5;
 let sayac_id;
+let ses_menu_btn = document.getElementById("ses-btn");
+let video_menu_btn = document.getElementById("video-btn");
+let tema_menu_btn = document.getElementById("tema-btn");
+let pomodoro_menu_btn = document.getElementById("pomodoro-menu-btn");
+let ses_menu_kapat_btn = document.getElementById("ses-menu-kapat");
+let video_menu_kapat_btn = document.getElementById("video-menu-kapat");
+let tema_menu_kapat_btn = document.getElementById("tema-menu-kapat");  
+let pomodoro_menu_kapat_btn = document.getElementById("pomodoro-menu-kapat");
+let ses_menu = document.getElementById("ses-menu");
+let video_menu = document.getElementById("video-menu");
+let tema_menu = document.getElementById("tema-menu");
+let pomodoro_menu = document.getElementById("pomodoro-menu");
+let kronometre_btn = document.getElementById("kronometre-btn");
+let kronometre = document.getElementById("kronometre");
+let kronometre_butonlar = document.getElementById("kronometre-butonlar");
+let pomodoro_sayac = document.getElementById("sayac");
+let pomodoro_sayac_butonlar = document.getElementById("sayac-butonlar");
+
+kronometre_btn.addEventListener("click", () => {
+  kronometre_butonlar.hidden = !kronometre_butonlar.hidden;
+  kronometre.hidden = !kronometre.hidden;
+  pomodoro_sayac.hidden = !pomodoro_sayac.hidden;
+  pomodoro_sayac_butonlar.hidden = !pomodoro_sayac_butonlar.hidden;
+  if (kronometre_btn.innerText == "Kronometre") {
+    kronometre_btn.innerText = "Pomodoro";
+  }
+  else{
+    kronometre_btn.innerText = "Kronometre";
+  }
+})
+
+ses_menu_btn.addEventListener("click", () => {
+  ses_menu.hidden = false;
+  video_menu.hidden = true;
+  tema_menu.hidden = true;
+  pomodoro_menu.hidden = true;
+})
+ses_menu_kapat_btn.addEventListener("click", () => {
+  ses_menu.hidden = true;
+})
+
+video_menu_btn.addEventListener("click", () => {
+  video_menu.hidden = false;
+  ses_menu.hidden = true;
+  tema_menu.hidden = true;
+  pomodoro_menu.hidden = true;
+})
+video_menu_kapat_btn.addEventListener("click", ()=>{
+  video_menu.hidden = true;
+})
+
+tema_menu_btn.addEventListener("click", () => {
+  tema_menu.hidden = false;
+  ses_menu.hidden = true;
+  video_menu.hidden = true;
+  pomodoro_menu.hidden = true;
+})
+tema_menu_kapat_btn.addEventListener("click", () => {
+  tema_menu.hidden = true;
+})
+
+pomodoro_menu_btn.addEventListener("click", () => {
+  pomodoro_menu.hidden = false;
+  ses_menu.hidden = true;
+  video_menu.hidden = true;
+  tema_menu.hidden = true;
+})
+pomodoro_menu_kapat_btn.addEventListener("click", () => {
+  pomodoro_menu.hidden = true;
+})
+
+
+
+let title = document.querySelector("title");       // title DOM elementini seçtik
+
+
+const config = { attributes: true };              // attribute'u ayarlanabilir/değiştirilebilir kıldık 
+
+// observer mutasyon(config nesnesindeki özelliklerde) gözlediğinde çalışan fonksiyon
+function callback(){
+  let dakikaTitle = dakika.style.getPropertyValue("--value");   // sürekli değişen dakikayı getproperty fonksiyonu ile alıp dakikaTitle değişkenine eşitliyoruz
+  let saniyeTitle = saniye.style.getPropertyValue("--value");  // aynı işi saniye için de yapıyoruz
+  if (saniyeTitle == 0) {
+    saniyeTitle = "00";
+  }
+  if (dakikaTitle == 0) {
+    dakikaTitle = "00";
+  }
+  title.innerText = `${dakikaTitle}:${saniyeTitle} - Pomodoro2Do`;  // title'ın innertext'ini yukarıda eşitleme yaptığımız değişkenlerle değiştiriyoruz
+};
+
+// üstteki callback fonksiyonunu çağıran observer objesini oluşturma
+const observer = new MutationObserver(callback);
+
+// observer objesinin saniye elementinin config içindeki özelliklerinin değişimini incelemeye başlamasını sağladık. Bu özellikler her değiştiğinde callback fonksiyonu çağırılacak
+observer.observe(saniye, config);
 
 pomodoro_input.addEventListener("change", function () {
   pomodoro_baslangic = pomodoro_input.value;
   if (calisma_vakti) {
     dakika.style.setProperty("--value", parseInt(pomodoro_input.value));
     saniye.style.setProperty("--value", 0);
+    
   }
 })
 
@@ -58,7 +155,7 @@ mola_input.addEventListener("change", function () {
   }
 })
 
-calisma_mola_btn.addEventListener("click", function () {
+calisma_mola_btn.addEventListener("click", function () {  //çalışma-mola butonu event listener
 
     clearInterval(sayac_id);
     sayac_id = undefined;     
