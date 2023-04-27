2e5d64417fc114504a633d17323fd9370fd44566
eWosMer <aymuhammedmert@gmail.com>
2023-04-28 01:41:46 +0300

    menü butonları aktifleştirildi ve tema,ses,video geçişleri sağlandı

--- theme.js
+++ theme.js
@@ -1,9 +1,27 @@
-const temalar = [pastel,light,dark,cupcake,bumblebee,emerald,corporate,
-                synthwave,retro,cyberpunk,valentine,halloween,garden,forest,
-                aqua,wireframe,black,luxury,draclua,cmyk,autumn,business,acid,
-                lemonade,night,coffee,];
-
-
-//black oldu ama logolar beyaz olabilir!?
-//luxury çıkartılabilir
+/*  const temalar = [pastel,light,dark,cupcake,bumblebee,emerald,corporate,
+                    synthwave,retro,cyberpunk,valentine,halloween,garden,forest,
+                     aqua,wireframe,black,luxury,draclua,cmyk,autumn,business,acid,
+                    lemonade,night,coffee,]; 
+*/
 //26 tane var
+let html = document.querySelector("html");
+
+let tema = localStorage.getItem("theme");
+if (tema) {
+  html.setAttribute("data-theme", tema);
+}
+
+let tema_butonlari = document.querySelectorAll(".tema-btn");
+tema_butonlari.forEach((tema_btn) => {
+  tema_btn.addEventListener("click", () => {
+    let yeni_tema = tema_btn.innerText.toLowerCase();
+    html.setAttribute("data-theme", yeni_tema);
+    localStorage.setItem("theme", yeni_tema);
+  })
+})
+
+
+
+
+
+
